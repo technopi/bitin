78019decb00c0c90586616af0ac9cfa04f0d8a75
Aditya Raj <115471257+hexronuspi@users.noreply.github.com>
2024-01-06 17:53:45 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bitin',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    # Additional metadata
+    author='Aditya',
+    description='Digital Electronics Library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/hexronuspi/bitin/',
+)
